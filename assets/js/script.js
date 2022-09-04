06bc1554e97aacf22f804ea4bef0071b19260039
Argent <jrargent42@gmail.com>
2022-09-04 13:23:26 -0400

    set up API key, test API fetch, set up function for grabbing lat and lon

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,14 @@
 /* notes on how to handle pulling and presenting weather data
 
+
+Geocoding API:
+
+http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+
+One call API:
+
+https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+
 Pull input data from search bar, insert into call for weather data 
 read api doc to see how to convert lat/long
 
@@ -42,3 +51,51 @@ Breakdown of 1.
 
 
  */
+
+var appid = "8e0314ddd129d7fc7871d6bdb2b3bf0a";
+var userFormEl = document.querySelector('#user-form');
+var cityInputEl = document.querySelector("#city");
+var cityCurrentEl = document.querySelector("#city-current");
+
+// Step 1 - grab city name from user
+var formSubmitHandler = function (event) {
+    event.preventDefault();
+  
+    var cityName = cityInputEl.value.trim(); // should add in split by space/strip out commas to grab state name as well as city name. Create variable for statename as well as city name
+  
+    if (cityName) {
+        console.log(cityName);
+      getCityData(cityName); // This needs to pass the city name into the geocoder api
+  
+      cityCurrentEl.textContent = ''; // this should clear out the div with the current city info. If not, update and change.
+      cityInputEl.value = ''; // clears out text input
+    } else {
+      alert('Please enter a city name');
+    }
+  };
+
+  var getCityData = function(city) {
+    // geocoding api - will give latitude/longitude for given city
+    var apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=" + appid; // check docs about using string interpolation here instead
+
+
+    fetch(apiUrl)
+    .then(function (response) {
+      if (response.ok) {
+        console.log(response);
+        response.json().then(function (data) {
+          console.log(data);
+          console.log(data[0].lat, data[0].lon);
+        //   displayCityWeather(data, city); <-- need to create function for taking lat and long data and doing next API fetch
+        });
+      } else {
+        alert('Error: ' + response.statusText);
+      }
+    })
+    .catch(function (error) {
+      alert('Unable to connect to Geocoding API');
+    });
+};
+  
+
+  userFormEl.addEventListener('submit', formSubmitHandler);
